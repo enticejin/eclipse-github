341bafa9931ea7a2827171b2cbf8ba2e468dabe4
Administrator <www.403367632@qq.com>
2020-06-18 09:48:56 +0800

    添加layui

---
+++ lay-ui/src/main/java/com/example/lay/LayUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.lay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayUiApplication.class, args);
+	}
+
+}
