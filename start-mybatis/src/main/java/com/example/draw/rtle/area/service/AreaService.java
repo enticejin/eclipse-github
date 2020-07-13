77ca74c28af76e8ac5fddf12626879de11d31fca
Administrator <www.403367632@qq.com>
2020-07-13 11:20:03 +0800

    Area Jpa

--- start-mybatis/src/main/java/com/example/draw/rtle/area/service/AreaService.java
+++ start-mybatis/src/main/java/com/example/draw/rtle/area/service/AreaService.java
@@ -1,29 +1,56 @@
 package com.example.draw.rtle.area.service;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
 
 import com.example.draw.rtle.area.model.Area;
 
+
 /** 
-* @version 创建时间：2020年6月16日 上午9:21:18
+* @version 创建时间：2020年7月10日 下午3:16:12
 */
 public interface AreaService {
-	int deleteByPrimaryKey(Integer id);
-
-    int insert(Area record);
-
-    int insertSelective(Area record);
-
-    Area selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(Area record);
-
-    int updateByPrimaryKey(Area record);
-
-	List<Area> getAreaLsit();
-
-	Map<String, Object> areaList(Map<String, Object> map);
-
-	List<Area> getAreaByName(String areaName);
+	/*
+	 *获取所有文件信息
+	 */
+	public List<Area> findAll();
+	/*
+	 * 保存
+	 */
+	public void save(Area area);
+	
+	/*
+	 * 删除
+	 */
+	public void delete(int id);
+	/*
+	 * 根据id查询
+	 */
+	public Area getOne(int id);
+	/*
+	 * 根据id查询
+	 */
+	public Optional<Area>  findById(int id);
+	/**
+	 * 分页
+	 * @param Area
+	 * @param page
+	 * @param limit
+	 * @return
+	 */
+	public List<Area> list(Area area, int page, int limit);
+	/**
+	 * 获取总数
+	 * @param Area
+	 * @return
+	 */
+	public Long getCount(Area area);
+	/**
+	 * 按名称查询区域
+	 * @param areaName
+	 * @return
+	 */
+	public List<Area> getAreaByName(String areaName);
 }
--- start-mybatis/src/main/java/com/example/draw/rtle/area/service/impl/AreaServiceImpl.java
+++ start-mybatis/src/main/java/com/example/draw/rtle/area/service/impl/AreaServiceImpl.java
@@ -1,77 +1,90 @@
 package com.example.draw.rtle.area.service.impl;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.example.draw.rtle.area.mapper.AreaMapper;
 import com.example.draw.rtle.area.model.Area;
 import com.example.draw.rtle.area.service.AreaService;
-import com.example.draw.rtle.utils.PageBean;
+import com.example.draw.rtle.userFile.model.UserFile;
 
 /** 
-* @version 创建时间：2020年6月16日 上午9:22:14
+* @version 创建时间：2020年7月10日 下午3:49:42
 */
 @Service
 public class AreaServiceImpl implements AreaService {
 	@Autowired
 	private AreaMapper areaMapper;
 	@Override
-	public int deleteByPrimaryKey(Integer id) {
+	public List<Area> findAll() {
 		
-		return areaMapper.deleteByPrimaryKey(id);
+		return areaMapper.findAll();
 	}
 
 	@Override
-	public int insert(Area record) {
-		
-		return areaMapper.insert(record);
+	public void save(Area area) {
+		areaMapper.save(area);
 	}
 
 	@Override
-	public int insertSelective(Area record) {
-		
-		return areaMapper.insertSelective(record);
+	public void delete(int id) {
+		areaMapper.deleteById(id);
+
 	}
 
 	@Override
-	public Area selectByPrimaryKey(Integer id) {
+	public Area getOne(int id) {
 		
-		return areaMapper.selectByPrimaryKey(id);
+		return areaMapper.getOne(id);
 	}
 
 	@Override
-	public int updateByPrimaryKeySelective(Area record) {
+	public Optional<Area> findById(int id) {
 		
-		return areaMapper.updateByPrimaryKeySelective(record);
+		return areaMapper.findById(id);
 	}
 
 	@Override
-	public int updateByPrimaryKey(Area record) {
-		
-		return areaMapper.updateByPrimaryKey(record);
+	public List<Area> list(Area area, int page, int pageSize) {
+		Pageable pageable = PageRequest.of(page-1,pageSize,Sort.Direction.DESC,"id");
+		Page<Area> pageUserFile = areaMapper.findAll(new Specification<Area>() {
+			@Override
+			public javax.persistence.criteria.Predicate toPredicate(Root<Area> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				javax.persistence.criteria.Predicate predicate=cb.conjunction();
+				predicate.getExpressions().add(cb.equal(root.get("flag"), 0));
+				return predicate;
+			}
+		}, pageable);
+		return pageUserFile.getContent();
 	}
 
 	@Override
-	public List<Area> getAreaLsit() {
-		
-		return areaMapper.getAreaLsit();
-	}
-
-	@Override
-	public Map<String, Object> areaList(Map<String, Object> map) {
-		List<Area> areaList =  areaMapper.areaList(map);
-		int count = areaMapper.count(map);
-		PageBean.serMap(map, count, areaList, Area.class);
-		return map;
+	public Long getCount(Area area) {
+		return areaMapper.count(new Specification<Area>() {
+			@Override
+			public javax.persistence.criteria.Predicate toPredicate(Root<Area> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				javax.persistence.criteria.Predicate predicate = cb.conjunction();
+				predicate.getExpressions().add(cb.equal(root.get("flag"), 0));
+				return predicate;
+			}
+		});
 	}
 
 	@Override
 	public List<Area> getAreaByName(String areaName) {
-		
-		return areaMapper.getAreaByName(areaName);
+		return areaMapper.findByAreaName(areaName);
 	}
 
 }
